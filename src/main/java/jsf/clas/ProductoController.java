f9130090903151f7a0b3702f87a76160c6cf1dcd
David Villeda <42588903+DavidITT@users.noreply.github.com>
2020-04-21 17:49:54 -0500

    Diseño de Productos

--- src/main/java/jsf/clas/ProductoController.java
+++ src/main/java/jsf/clas/ProductoController.java
@@ -76,17 +76,17 @@ public class ProductoController implements Serializable {
     public String prepareCreate() {
         current = new Producto();
         selectedItemIndex = -1;
-        return "Create";
+        return "List";
     }
 
     public String create() {
         try {
             getFacade().create(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoCreated"));
+            JsfUtil.addSuccessMessage("¡Producto creado con exito!");
             return prepareCreate();
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
+            JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
+            return "List";
         }
     }
 
@@ -97,13 +97,13 @@ public class ProductoController implements Serializable {
     }
 
     public String update() {
-        try {
+         try {
             getFacade().edit(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoUpdated"));
-            return "View";
+            JsfUtil.addSuccessMessage("¡Producto editado con exito!");
+            return "List";
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
+            JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
+            return "List";
         }
     }
 
@@ -132,9 +132,10 @@ public class ProductoController implements Serializable {
     private void performDestroy() {
         try {
             getFacade().remove(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoDeleted"));
+            JsfUtil.addSuccessMessage("¡Producto eliminado con exito!");
+            
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+          JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
         }
     }
 
