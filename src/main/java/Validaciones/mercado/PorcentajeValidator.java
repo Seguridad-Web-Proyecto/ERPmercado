e719857e5be34a1c4f45fbf6b5a401eb1695ab53
Katia Manjarrez <kmanjarrez67@gmail.com>
2020-04-26 12:42:45 -0500

    VALIDACIONES  porcentaje

---
+++ src/main/java/Validaciones/mercado/PorcentajeValidator.java
@@ -0,0 +1,46 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Validaciones.mercado;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author Dann
+ */
+public class URLValidator implements Validator
+{
+
+    private static final String URL_PATTERN = "/^((100(\.0{1,2})?)|(\d{1,2}(\.\d{1,2})?))$/";
+
+    private final Pattern pattern;
+    private Matcher matcher;
+
+    public URLValidator()
+    {
+        pattern = Pattern.compile(URL_PATTERN);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException
+    {
+        matcher = pattern.matcher(value.toString());
+        if (!matcher.matches())
+        {
+
+            FacesMessage msg = new FacesMessage("Error... URL no válido", "Invalid format..");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+
+}
